9f98f1f19706c5589e215f4f9408ddad56f55f2f
Rodrey Mark Goite <rodrey@ebi.ac.uk>
2018-04-12 12:26:13 +0100

    Updated to use environment variable if it's present.

--- BundleService/__init__.py
+++ BundleService/__init__.py
@@ -1,16 +1,22 @@
+import os
 import requests
 import json
 
-# TODO replace this to use environment variable instead
-API_URL = 'https://dss.dev.data.humancellatlas.org/v1'
+API_URL_DEFAULT = 'https://dss.dev.data.humancellatlas.org/v1'
+API_URL_ENV_KEY = 'CT_DSS_API'
 
 class BundleService:
+
+    def __init__(self):
+        self.base_url = API_URL_DEFAULT
+        if API_URL_ENV_KEY in os.environ:
+            self.base_url = os.environ[API_URL_ENV_KEY]
 
     def get_files(self, bundle_uuid):
         bundle = self.get_bundle(bundle_uuid)
         return [file['uuid'] for file in bundle['files']]
 
     def get_bundle(self, uuid):
-        bundle_url = '%s/bundles/%s' % (API_URL, uuid)
+        bundle_url = '%s/bundles/%s' % (self.base_url, uuid)
         response = requests.get(bundle_url, {'replica': 'aws'})
         return json.loads(response.text)['bundle']
