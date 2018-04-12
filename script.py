864b8cf00741c9b198961b2d5e9e3d4809a0d21b
Rodrey Mark Goite <rodrey@ebi.ac.uk>
2018-04-12 15:58:13 +0100

    Defined script for putting together all files in a bundle.

--- script.py
+++ script.py
@@ -1,2 +1,11 @@
-import requests
+import sys
+import json
+from BundleService import BundleService
 
+if __name__ == '__main__':
+    service = BundleService()
+    bundle_uuid = sys.argv[1]
+    contents = []
+    for file in service.get_indexed_files(bundle_uuid):
+        contents.append(file)
+    print(json.dumps(contents, indent=4))
