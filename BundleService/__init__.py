a5b1309b90529dad2e50987401afea32235e33d4
Rodrey Mark Goite <rodrey@ebi.ac.uk>
2018-04-12 12:12:55 +0100

    Defined method to extract File id's from bundle.

--- BundleService/__init__.py
+++ BundleService/__init__.py
@@ -1,10 +1,16 @@
 import requests
+import json
 
 # TODO replace this to use environment variable instead
 API_URL = 'https://dss.dev.data.humancellatlas.org/v1'
 
 class BundleService:
 
+    def get_files(self, bundle_uuid):
+        bundle = self.get_bundle(bundle_uuid)
+        return [file['uuid'] for file in bundle['files']]
+
     def get_bundle(self, uuid):
         bundle_url = '%s/bundles/%s' % (API_URL, uuid)
-        return requests.get(bundle_url, {'replica': 'aws'})
+        response = requests.get(bundle_url, {'replica': 'aws'})
+        return json.loads(response.text)['bundle']
