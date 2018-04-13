39863f548386aefd9ee0f0c0757bfdbd689d6800
Rodrey Mark Goite <rodrey@ebi.ac.uk>
2018-04-13 11:58:40 +0100

    Updated Core service to use Environment config.

---
+++ Environment/__init__.py
@@ -0,0 +1,9 @@
+class Environment:
+
+    def __init__(self, dss_api_url, core_api_url):
+        self.dss_api_url = dss_api_url
+        self.core_api_url = core_api_url
+
+
+DEV = Environment('https://dss.dev.data.humancellatlas.org/v1', 'http://api.ingest.dev.data.humancellatlas.org')
+PROD = Environment('https://dss.data.humancellatlas.org/v1', 'http://api.ingest.dev.data.humancellatlas.org')
--- IngestCoreService/__init__.py
+++ IngestCoreService/__init__.py
@@ -1,11 +1,15 @@
 import requests
 import json
-
-API_URL_DEFAULT = 'http://api.ingest.dev.data.humancellatlas.org'
+import Environment
 
 class IngestCoreService:
 
+    def __init__(self, env=None):
+        self.env = env
+        if not env:
+            self.env = Environment.DEV
+
     def get_bundle_manifests(self, envelope_uuid):
-        url = '%s/submissionEnvelopes/%s/bundleManifests' % (API_URL_DEFAULT, envelope_uuid)
+        url = '%s/submissionEnvelopes/%s/bundleManifests' % (self.env.core_api_url, envelope_uuid)
         response = requests.get(url)
         return json.loads(response.text)['_embedded']['bundleManifests']
