afce4fac1b7a0a7882203efc60636eab1ed89860
Rodrey Mark Goite <rodrey@ebi.ac.uk>
2018-04-13 14:15:47 +0100

    Refactored clear tape script.

--- clear-tape.py
+++ clear-tape.py
@@ -21,22 +21,48 @@ def determine_env():
 def run():
     env_config = determine_env()
     core_service = IngestCoreService(env_config)
-    bundle_service = BundleService(env_config)
 
+    output_directory = './output'
+    prepare_output_directory(output_directory)
+    output_prefix = determine_output_prefix(output_directory)
+
+    submission_id = determine_submission_id()
+    bundle_manifests = core_service.get_bundle_manifests(submission_id)
+
+    bundle_service = BundleService(env_config)
+    process_bundles(bundle_service, bundle_manifests, output_prefix)
+
+
+def prepare_output_directory(output_directory):
+    if not os.path.exists(output_directory):
+        os.makedirs(output_directory)
+
+
+def determine_output_prefix(output_directory):
     prefix = 'bundle'
     if len(sys.argv) == 3:
         prefix = sys.argv[2]
+    output_prefix = '%s/%s' % (output_directory, prefix)
+    return output_prefix
 
-    submission_id = sys.argv[1]
-    bundle_manifests = core_service.get_bundle_manifests(submission_id)
+
+def determine_submission_id():
+    if not len(sys.argv) < 2:
+        submission_id = sys.argv[1]
+        return submission_id
+    else:
+        raise Exception('Submission ID required.')
+
+
+def process_bundles(bundle_service, bundle_manifests, output_prefix):
     count = 1
     for manifest in bundle_manifests:
         bundle_uuid = manifest['bundleUuid']
         print('Preparing bundle #%d with id [%s]...' % (count, bundle_uuid))
         bundle = compile_bundle(bundle_service, bundle_uuid)
-        file_name = '%s_%d.json' % (prefix, count)
+        file_name = '%s_%d.json' % (output_prefix, count)
         with open(file_name, 'w+') as file:
-            file.write(json.dumps(bundle))
+            file.write(json.dumps(bundle, indent=2))
             count = count + 1
             print('done')
 
