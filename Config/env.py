1784e1528dec6400620b6554ef93a0c2afcd72eb
Rodrey Mark Goite <rodrey@ebi.ac.uk>
2018-04-13 12:07:20 +0100

    Restrutured configuration scripts.

--- Config/env.py
+++ Config/env.py
@@ -4,6 +4,5 @@ class Environment:
         self.dss_api_url = dss_api_url
         self.core_api_url = core_api_url
 
-
 DEV = Environment('https://dss.dev.data.humancellatlas.org/v1', 'http://api.ingest.dev.data.humancellatlas.org')
 PROD = Environment('https://dss.data.humancellatlas.org/v1', 'http://api.ingest.dev.data.humancellatlas.org')
--- IngestCoreService/__init__.py
+++ IngestCoreService/__init__.py
@@ -1,13 +1,13 @@
 import requests
 import json
-import Environment
+from Config import env
 
 class IngestCoreService:
 
-    def __init__(self, env=None):
-        self.env = env
-        if not env:
-            self.env = Environment.DEV
+    def __init__(self, _env=None):
+        self.env = _env
+        if not _env:
+            self.env = env.DEV
 
     def get_bundle_manifests(self, envelope_uuid):
         url = '%s/submissionEnvelopes/%s/bundleManifests' % (self.env.core_api_url, envelope_uuid)
