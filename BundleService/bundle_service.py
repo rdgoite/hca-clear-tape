4c2adf7d51b98a780539e7fb9b28b94b449be5bd
Rodrey Goite <rodrey@ebi.ac.uk>
2018-11-30 11:57:53 +0000

    Moved BundleService class to its own source file.

---
+++ BundleService/bundle_service.py
@@ -0,0 +1,22 @@
+import requests
+
+from Config import env
+
+
+class BundleService:
+
+    def __init__(self, _env=env.DEV):
+        self.env = _env
+
+    def iterate_indexed_files(self, bundle_uuid):
+        bundle = self.get_bundle(bundle_uuid)
+        for file in bundle['files']:
+            if (file['indexed']):
+                file_url = '%s/files/%s' % (self.env.dss_api_url, file['uuid'])
+                response = requests.get(file_url, {'replica': 'aws'})
+                yield response.json()
+
+    def get_bundle(self, uuid):
+        bundle_url = '%s/bundles/%s' % (self.env.dss_api_url, uuid)
+        response = requests.get(bundle_url, {'replica': 'aws'})
+        return response.json()['bundle']
