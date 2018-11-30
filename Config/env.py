1365cdf04982d85e6eafb431e17e668e262f4758
Rodrey Goite <rodrey@ebi.ac.uk>
2018-11-30 15:38:28 +0000

    Added staging env.

--- Config/env.py
+++ Config/env.py
@@ -4,10 +4,12 @@ class Environment:
         self.dss_api_url = dss_api_url
         self.core_api_url = core_api_url
 
-DEV = Environment('https://dss.dev.data.humancellatlas.org/v1', 'http://api.ingest.dev.data.humancellatlas.org')
-PROD = Environment('https://dss.data.humancellatlas.org/v1', 'http://api.ingest.data.humancellatlas.org')
 
-MAP = {
-    'DEV': DEV,
-    'PROD': PROD
-}
+def _create_env(env_name):
+    return Environment(f'https://dss.{env_name}.data.humancellatlas.org/v1',
+                       f'http://api.ingest.{env_name}.data.humancellatlas.org')
+
+
+MAP = {env_name.upper(): _create_env(env_name) for env_name in ['dev', 'staging']}
+MAP['PROD'] = Environment('https://dss.data.humancellatlas.org/v1',
+                          'http://api.ingest.data.humancellatlas.org')
